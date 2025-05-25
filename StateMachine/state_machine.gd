368dd144fde7532009ebd61bb14b05b874b9ee50
Galit Weinfeld <galitdances@gmail.com>
2025-05-24 20:03:28 -0400

    Added charge state

--- StateMachine/state_machine.gd
+++ StateMachine/state_machine.gd
@@ -2,7 +2,7 @@ extends Node
 
 var current_state : State
 var states : Dictionary = {}
-var state_choices = ["wander", "chase", "idle", "punch", "block"]
+var state_choices = ["wander", "chase", "idle", "punch", "block", "charge"]
 @export var monster = CharacterBody2D
 
 

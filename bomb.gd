8a623a81d32fc57e89651049a5a19113ad5b0c2e
Galit Weinfeld <galitdances@gmail.com>
2025-08-28 20:23:50 -0400

    Fixed bomb rotation

--- bomb.gd
+++ bomb.gd
@@ -8,10 +8,11 @@ var overlapping_areas : Array[Area2D]
 var exploding = false
 
 var speed: float = 200.0
-var spin_speed: float = 5.0
+var spin_speed: float = 40.0
 var target: Vector2
 var direction: Vector2
 var spin_sign: float
+var distance: float
 
 @onready var monster : Monster
 
@@ -19,29 +20,27 @@ var spin_sign: float
 func _ready():
 	if monster.facing == "right":
 		direction = Vector2(-1, 0)
-		target = position - Vector2(100,0)
+		target = position - Vector2(randi_range(50,125),0)
+		spin_sign = -1
 	else:
 		direction = Vector2(1, 0)
-		target = position + Vector2(100,0)
+		target = position + Vector2(randi_range(50,125),0)
+		spin_sign = 1
+	distance = position.distance_to(target)
+	$ExplosionCountdown.wait_time = randf_range(3,5)
 	$ExplosionCountdown.start()
 
 
 func _physics_process(delta: float) -> void:
-	var goal = target - position
-	if goal.length() > 1.0:
-		position += direction * speed * delta
-		if direction.x >= 0.0:
-			spin_sign = 1.0
-		else:
-			spin_sign = -1.0
-		rotation += spin_speed * spin_sign * delta
+	var decay = position.distance_to(target) / distance
+	position.x = move_toward(position.x, target.x, speed * delta)
+	rotation += spin_speed * spin_sign * delta * decay
 	
 	if exploding:
 		overlapping_areas = $Area2D.get_overlapping_areas()
 		for area in overlapping_areas:
 			if area.name == "hurtbox":
 				queue_free()
-
 
 func _on_explosion_countdown_timeout():
 	$Area2D/ExplosionHitBox.disabled = false

6bd2c86a43883dba00ef6123be0015510174fa1e
raamweinfeld <raamweinfeld@gmail.com>
2025-09-08 15:56:08 -0400

    Fixed hurt flash

--- StateMachine/hurt.gd
+++ StateMachine/hurt.gd
@@ -7,8 +7,8 @@ func Enter():
 	monster.velocity = Vector2.ZERO
 	monster.animation_player.play("hurt")
 	
-	monster.get_node("root").modulate = Color.RED
-	get_tree().create_tween().tween_property(monster.get_node("root"), "modulate", Color.WHITE, 0.3).set_trans(Tween.TRANS_BOUNCE)
+	monster.root.modulate = Color("ff0e1b")
+	get_tree().create_tween().tween_property(monster.root, "modulate", Color.WHITE, 0.3).set_trans(Tween.TRANS_BOUNCE)
 
 func animation_finished(anim_name: String):
 	if monster.current_hp <= 0:

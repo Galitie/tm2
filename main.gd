ebe8659595ddeda6e5075ecd0d8146bcafed554b
Galit Weinfeld <galitdances@gmail.com>
2025-09-20 01:15:04 -0400

    Added more name variation

--- main.gd
+++ main.gd
@@ -454,7 +454,7 @@ func spawn_poop(monster):
 	if monster.player.poop_summons:
 		poop.is_a_summon = true
 	if monster.player.larger_poops:
-		poop.scale += Vector2(.50, .50)
+		poop.scale += Vector2(randf_range(.50,.65), randf_range(.50,.65))
 	add_child(poop)
 	poop.add_to_group("CleanUp")
 

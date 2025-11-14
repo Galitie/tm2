84c769218e7f84adb7e5366998e1f6d9922f0d97
Galit Weinfeld <galitdances@gmail.com>
2025-11-13 20:04:58 -0500

    Added cards that unlock other cards and cards that remove cards

--- Cards/card_info.gd
+++ Cards/card_info.gd
@@ -21,3 +21,5 @@ enum Attributes {NONE, HP, MOVE_SPEED, BASE_DAMAGE, REROLL, UPGRADE_POINTS, CRIT
 @export var attribute_3 : Attributes
 @export var attribute_label_3 : String
 @export var attribute_amount_3: float
+@export var unlocked_cards: Array[CardResourceScript]
+@export var remove_cards: Array[CardResourceScript]

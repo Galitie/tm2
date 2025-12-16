49abcdf3bb8486e7f4f0e8cc987ac30c875ee9a2
Galit Weinfeld <galitdances@gmail.com>
2025-12-15 21:49:23 -0500

    Added dance animation for end of round celebration

--- states/dance.gd
+++ states/dance.gd
@@ -6,7 +6,7 @@ var monster: CharacterBody2D
 #TODO: Raam, add dance animation here
 func Enter():
 	monster.velocity = Vector2.ZERO
-	monster.animation_player.play("idle")
+	monster.animation_player.play("dance")
 
 func animation_finished(anim_name: String) -> void:
 	pass

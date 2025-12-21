2c28367a3f7b9a7dbbcfdf4cd6cf2f8c0a5df21f
Galit Weinfeld <galitdances@gmail.com>
2025-12-20 22:51:21 -0500

    Added sound for bullet missing

--- projectiles/projectile.gd
+++ projectiles/projectile.gd
@@ -14,12 +14,14 @@ var emitter
 
 func _ready() -> void:
 	lifespan = randf_range(1,1.5)
+	$AudioStreamPlayer.stream = load("uid://c231yi1ewk36s")
 
 
 func _physics_process(delta: float) -> void:
 	global_position += direction * speed * delta
 	life += delta * 1.0
 	if life > lifespan:
+		$AudioStreamPlayer.play()
 		$Area2D/CollisionShape2D.disabled = true
 		$Sprite2D.texture = pop_texture
 		await get_tree().create_timer(.05).timeout
